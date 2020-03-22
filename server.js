baf2016f6a60de10b8b3a79d7a8040a135d39a6e
noandasan <n_oandasan@yahoo.com>
2020-03-22 10:10:56 +0300

    u
    u

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.use(function(req, res, next){
 // }).listen(5000);
 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
 });
