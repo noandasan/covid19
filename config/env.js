e95b381274e44be86d1923646aa40387405f5817
noandasan <n_oandasan@yahoo.com>
2020-03-29 12:34:24 +0300

    u
    u

--- config/env.js
+++ config/env.js
@@ -1,15 +1,8 @@
 const env = {
-<<<<<<< HEAD
-  database: 'covid19',
+ database: 'covid19',
   username: 'covid19',
   password: 'covid19',
   host: '94.97.246.47',
-=======
-  database: 'smharabi_covid',
-  username: 'smharabi_covid',
-  password: '.Cn;n]%n)ivP',
-  host: '108.179.237.41',
->>>>>>> 5fb9b6941499d604a8f018b20a8f2d6e87c5d91a
   dialect: 'mysql',
 	port: 3306,
   pool: {
